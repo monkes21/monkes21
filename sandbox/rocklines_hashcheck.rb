83a18c5bc3a0159b5685a9974d91242835d4950f
Gerald Bauer <gerald.bauer@gmail.com>
2024-02-15 21:27:36 +0100

    up

---
+++ sandbox/rocklines_hashcheck.rb
@@ -0,0 +1,49 @@
+###
+#  to run use:
+#    $ ruby sandbox/rocklines_hashcheck.rb
+
+
+$LOAD_PATH.unshift( "../../ordbase/ordbase/ordinals/lib" )
+require 'ordinals'
+
+
+missing = []
+
+
+ids = (0..20).to_a
+ids.each do |id|
+    path = "./rocklines/hashcheck/#{id}.json"
+
+    if File.exist?( path )
+        ## skip; already checked
+    else
+        ## note: start counting at 0/0
+      puts "==> #{id+1}/#{ids.size}..."
+      blob = read_blob( "./rocklines/inscribe/rockline#{id}.png" )
+      hash = Digest::SHA256.hexdigest( blob )
+
+      data = Ordinalsbot.hashcheck( hash )
+
+      puts "  #{data['status']} #{data['count']}"
+
+      if data['status'] != 'ok'
+        puts "!! ERROR - expected status ok, got:"
+        pp data
+        exit 1
+      end
+
+           ## note: only write-out - if count greater one!!!
+      if data['count'] > 0
+           write_json( path, data )    
+      else
+           missing << id
+      end
+    end
+end
+
+
+puts "  #{missing.size} missing item(s) found:"
+pp missing
+
+
+puts "bye"
