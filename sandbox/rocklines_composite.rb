c581a045318e301b04e94922c26cea136b39024a
Gerald Bauer <gerald.bauer@gmail.com>
2024-02-16 01:38:19 +0100

    up

---
+++ sandbox/rocklines_composite.rb
@@ -0,0 +1,25 @@
+require 'pixelart'
+
+## update fam composite via /inscribe images
+
+
+ids  = [
+    0, 1, 2, 3, 6, 7, 8, 
+    9, 10, 11, 12, 14, 17, 19
+]
+
+cols = 7
+rows = ids.size / cols 
+rows += 1    if ids.size % cols != 0
+fam = ImageComposite.new( cols, rows, height: 28, width: 28 )
+
+
+ids.each do |id|
+    fam << Image.read( "./rocklines/inscribe/rockline#{id}.png" )
+end
+
+fam.save( "./tmp/rocklines_mints.png" )
+fam.zoom(4).save( "./tmp/rocklines_mints@4x.png" )
+
+
+puts "bye"
