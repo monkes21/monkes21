00338a19d121d34727eeaa11b15693051dc9885d
Gerald Bauer <gerald.bauer@gmail.com>
2024-03-26 10:09:57 +0100

    up

---
+++ docs/goldcoins/items.js
@@ -0,0 +1,132 @@
+// items.js generated on 2024-03-25 07:13:48 UTC
+//   - mint count at 21
+
+ const mintCount = 21;
+ const items = [
+  {
+    "id": "4cea43a2a500ef996ef46e4b085dc9e4905c7815a6342c4352f08e5dc36347a3",
+    "name": "#0",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin0.png",
+    "inscriptionNumber": 65647472
+  },
+  {
+    "id": "7b296a179dc3a045de96005671d9757699357a8153ed747acf42f2e5bcf9b0ab",
+    "name": "#1",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin1.png",
+    "inscriptionNumber": 65631074
+  },
+  {
+    "id": "06bcf92cfcc8bcb487c3f65789050343d93cb84dfa998d06b084df2b81966533",
+    "name": "#2",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin2.png",
+    "inscriptionNumber": 65637919
+  },
+  {
+    "id": "06d6b59172ea1a264336b74c7f4255210335e8ddf2c2f3cb5664114576843f28",
+    "name": "#3",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin3.png",
+    "inscriptionNumber": 65730985
+  },
+  {
+    "id": "e3c57960c785e0e31d5444094390665bc9b3e14c7c31b1df5bbb0eb79e65237a",
+    "name": "#4",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin4.png",
+    "inscriptionNumber": 65731026
+  },
+  {
+    "id": "54f3ac0f0b7fb45861b95edb088354a9e07304e8726cd1d8310c2f6c246de3fc",
+    "name": "#5",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin5.png",
+    "inscriptionNumber": 65730849
+  },
+  {
+    "id": "8a056c1804e2038621a132f01fc81f4b19f49fce5f0492b2fd44310faed7c731",
+    "name": "#6",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin6.png",
+    "inscriptionNumber": 65730850
+  },
+  {
+    "id": "fb678f9b647c24ff02974ffc4f6442d9ca045ebc4d171055ecef73fef0370a24",
+    "name": "#7",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin7.png",
+    "inscriptionNumber": 65730854
+  },
+  {
+    "id": "c4c38eee09d778cb233ae8c17f1befbe1f296541a2dfb490619f2a17388505f7",
+    "name": "#8",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin8.png",
+    "inscriptionNumber": 65730855
+  },
+  {
+    "id": "20e13988a8156ca78760ad338c9d1fd034a3508d5e3e6d2346be44931d2eb98e",
+    "name": "#9",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin9.png",
+    "inscriptionNumber": 65730857
+  },
+  {
+    "id": "e4b2d53030fbc2c3a63921cec3ad566a8ca80064c3f2a2b85471738a9cde1357",
+    "name": "#10",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin10.png",
+    "inscriptionNumber": 65631112
+  },
+  {
+    "id": "6706f9921281a03c5d7f2632fdc5a54f91fb9c4e5ee4bfb901fd93014bcaa12e",
+    "name": "#11",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin11.png",
+    "inscriptionNumber": 65631097
+  },
+  {
+    "id": "0de991ff77b8e28a667c9d0418bcdf92ce60274c5e53936736229af38d26f58a",
+    "name": "#12",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin12.png",
+    "inscriptionNumber": 65731025
+  },
+  {
+    "id": "150b9adc9552cbaa39a50cc53bb72ffd9bf334c933edbcaaedeb229fb77282cd",
+    "name": "#13",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin13.png",
+    "inscriptionNumber": 65631071
+  },
+  {
+    "id": "7c67222790353df98cd64ce78e6d1b29aad8f5a2b2a3a16230a1c75b0d7d30db",
+    "name": "#14",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin14.png",
+    "inscriptionNumber": 65724223
+  },
+  {
+    "id": "8fd0869443a4407cae4688787735dfaa28075e5d01e9e5bc539a5cc0cbb1c846",
+    "name": "#15",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin15.png",
+    "inscriptionNumber": 65631091
+  },
+  {
+    "id": "073dc8db44fae4db36876b0ec7b1adc6ce5baf7c6e9e072bb267d3ec9ac3561b",
+    "name": "#16",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin16.png",
+    "inscriptionNumber": 65631075
+  },
+  {
+    "id": "c6472f2444e88e46804d0787cac12b68ccf091e4933bde5f223ff030a06e96d8",
+    "name": "#17",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin17.png",
+    "inscriptionNumber": 65631072
+  },
+  {
+    "id": "90db93593eb3077a9747c1e88f649d40966dcc7b733077048fd371de1e09df52",
+    "name": "#18",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin18.png",
+    "inscriptionNumber": 65730856
+  },
+  {
+    "id": "36da1d911aaf2241ab69e0f7eaaf94eb24eed686353195f97406e3955d67b553",
+    "name": "#19",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin19.png",
+    "inscriptionNumber": 65730858
+  },
+  {
+    "id": "997dfa9b09cdd18a8cc30f4315ee614e2ec8ab1736ed33d4b409f6efd4bec828",
+    "name": "#20",
+    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin20.png",
+    "inscriptionNumber": 65731027
+  }
+];
--- docs/items.js
+++ docs/items.js
@@ -1,132 +1,132 @@
-// items.js generated on 2024-03-25 07:13:48 UTC
-//   - mint count at 21
+// items.js generated on 2024-03-26 09:09:42 UTC
+//   - mint count at 0
 
- const mintCount = 21;
+ const mintCount = 0;
  const items = [
   {
-    "id": "4cea43a2a500ef996ef46e4b085dc9e4905c7815a6342c4352f08e5dc36347a3",
+    "id": "abe7a32e93d9c7431c6dee927cea2b8ed93229f2c74c7a60af98c473a13c6387",
     "name": "#0",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin0.png",
-    "inscriptionNumber": 65647472
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny0.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "7b296a179dc3a045de96005671d9757699357a8153ed747acf42f2e5bcf9b0ab",
+    "id": "d3433fa11bf7ab209e122b0e2da9fb2f17f2a62498d2db0e5e6acd8b0c9d3e60",
     "name": "#1",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin1.png",
-    "inscriptionNumber": 65631074
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny1.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "06bcf92cfcc8bcb487c3f65789050343d93cb84dfa998d06b084df2b81966533",
+    "id": "974dab31ed8972fde33e70a0301cb9e8d85618e999b7c577d569ceef4596aece",
     "name": "#2",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin2.png",
-    "inscriptionNumber": 65637919
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny2.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "06d6b59172ea1a264336b74c7f4255210335e8ddf2c2f3cb5664114576843f28",
+    "id": "25efc1c5e08b8de27392de8088f9424db2b8da6911318de549a736b845c5b36d",
     "name": "#3",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin3.png",
-    "inscriptionNumber": 65730985
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny3.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "e3c57960c785e0e31d5444094390665bc9b3e14c7c31b1df5bbb0eb79e65237a",
+    "id": "80f0b49defa550169caff878d9c827e8b7c7d170f458003b39e1580086a6db7a",
     "name": "#4",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin4.png",
-    "inscriptionNumber": 65731026
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny4.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "54f3ac0f0b7fb45861b95edb088354a9e07304e8726cd1d8310c2f6c246de3fc",
+    "id": "63f5cc07f34e81276314dfa92f697ff8522f4d30401b9818b5aabd534dd16306",
     "name": "#5",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin5.png",
-    "inscriptionNumber": 65730849
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny5.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "8a056c1804e2038621a132f01fc81f4b19f49fce5f0492b2fd44310faed7c731",
+    "id": "5db18c3adbb2d05607b7f3999e07d9079755ee0e618eb68a003329fa247bc3e8",
     "name": "#6",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin6.png",
-    "inscriptionNumber": 65730850
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny6.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "fb678f9b647c24ff02974ffc4f6442d9ca045ebc4d171055ecef73fef0370a24",
+    "id": "1a648fee7ea8b81b1805085776e95e1e912afd812493e47105def0cafbaf2c3f",
     "name": "#7",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin7.png",
-    "inscriptionNumber": 65730854
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny7.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "c4c38eee09d778cb233ae8c17f1befbe1f296541a2dfb490619f2a17388505f7",
+    "id": "c9bbca3d3cb9a50752a37c1dddc3988d5df7d8daa516182cf2f1494897731ed4",
     "name": "#8",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin8.png",
-    "inscriptionNumber": 65730855
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny8.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "20e13988a8156ca78760ad338c9d1fd034a3508d5e3e6d2346be44931d2eb98e",
+    "id": "efa57378ec61f00d42dd7f1875b78699ad586897f3b8e52492edcfe920e7634c",
     "name": "#9",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin9.png",
-    "inscriptionNumber": 65730857
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny9.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "e4b2d53030fbc2c3a63921cec3ad566a8ca80064c3f2a2b85471738a9cde1357",
+    "id": "dfb5aaf9c3e44e74082ef0b4d22a752700ca062aa7d2d6b42a6541d003b0b27b",
     "name": "#10",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin10.png",
-    "inscriptionNumber": 65631112
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny10.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "6706f9921281a03c5d7f2632fdc5a54f91fb9c4e5ee4bfb901fd93014bcaa12e",
+    "id": "354dd2ea52ddba56a1e1e4a389231d2c175ec3d45a326fac70321a088fe2219d",
     "name": "#11",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin11.png",
-    "inscriptionNumber": 65631097
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny11.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "0de991ff77b8e28a667c9d0418bcdf92ce60274c5e53936736229af38d26f58a",
+    "id": "3aa7f7beda8715f8edd97142ceeed6047ce5bbfb09d49290d89a13bfb2ff1a84",
     "name": "#12",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin12.png",
-    "inscriptionNumber": 65731025
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny12.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "150b9adc9552cbaa39a50cc53bb72ffd9bf334c933edbcaaedeb229fb77282cd",
+    "id": "d4be5776938baf1c7d91d87af07c4d7372011c7283aab51cff6675e9348d542f",
     "name": "#13",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin13.png",
-    "inscriptionNumber": 65631071
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny13.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "7c67222790353df98cd64ce78e6d1b29aad8f5a2b2a3a16230a1c75b0d7d30db",
+    "id": "bc66c4c3d5e9e550f588c488bee07700faee68ea7bdb03d048edb7285998ade7",
     "name": "#14",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin14.png",
-    "inscriptionNumber": 65724223
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny14.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "8fd0869443a4407cae4688787735dfaa28075e5d01e9e5bc539a5cc0cbb1c846",
+    "id": "abe46f6f8b42ad1adcd9990edd7c3d1fbe39b40fb02dd1cd581eb44054144300",
     "name": "#15",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin15.png",
-    "inscriptionNumber": 65631091
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny15.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "073dc8db44fae4db36876b0ec7b1adc6ce5baf7c6e9e072bb267d3ec9ac3561b",
+    "id": "b1247af4004b9e90fe2c165385fe4038890e56d8abe96a8786dc35211b70777e",
     "name": "#16",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin16.png",
-    "inscriptionNumber": 65631075
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny16.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "c6472f2444e88e46804d0787cac12b68ccf091e4933bde5f223ff030a06e96d8",
+    "id": "8353318cb6e3203bc15b43c2254cf7c50c3bb9bd7ed515d3131cb93cfb6c5783",
     "name": "#17",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin17.png",
-    "inscriptionNumber": 65631072
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny17.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "90db93593eb3077a9747c1e88f649d40966dcc7b733077048fd371de1e09df52",
+    "id": "31b52f70df79cf31672f1f257271f23dd341c14c780e62e43ab9ff2c3a9a685e",
     "name": "#18",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin18.png",
-    "inscriptionNumber": 65730856
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny18.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "36da1d911aaf2241ab69e0f7eaaf94eb24eed686353195f97406e3955d67b553",
+    "id": "efecaffc6ef05d9b880e6ea42ae4f00e1ed524836488c65c3fc3619015b799d8",
     "name": "#19",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin19.png",
-    "inscriptionNumber": 65730858
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny19.png",
+    "inscriptionNumber": 1
   },
   {
-    "id": "997dfa9b09cdd18a8cc30f4315ee614e2ec8ab1736ed33d4b409f6efd4bec828",
+    "id": "bbbcc718006656ae9bc451bec822e5107cb35da5ad83df5e3e422575bf4e8819",
     "name": "#20",
-    "image": "https://github.com/orc721/monkes21/raw/master/goldcoins/inscribe/goldcoin20.png",
-    "inscriptionNumber": 65731027
+    "image": "https://github.com/orc721/monkes21/raw/master/pennies/inscribe/penny20.png",
+    "inscriptionNumber": 1
   }
 ];
